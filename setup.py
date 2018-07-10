846b4f60f60c6c89d0f8b4311290d465b0f9b625
Anna-Livia <annalivia.gomart@gmail.com>
2018-07-10 17:41:43 +0200

    Ajoute les bases du pays avec le country template

---
+++ setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='OpenFisca-Mali',
+    version='0.0.3',
+    author='OpenFisca Team',
+    author_email='contact@openfisca.fr',
+    description=u'OpenFisca tax and benefit system for Mali',
+    keywords='benefit microsimulation social tax',
+    license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    url='https://github.com/openfisca/openfisca-country-template',
+    include_package_data=True,  # Will read MANIFEST.in
+    install_requires=[
+        'OpenFisca-Core >= 23.1, < 24.0',
+        ],
+    extras_require={
+        'test': [
+            'flake8 >= 3.4.0, < 3.5.0',
+            'flake8-print',
+            'nose',
+            ]
+        },
+    packages=find_packages(),
+    test_suite='nose.collector',
+    )
