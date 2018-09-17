dafb3a1d2cb75bd85945b4e7f1fd79655e4dc8b5
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-17 18:07:08 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Mali',
-    version='0.0.4',
+    version='0.0.5',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for Mali',
