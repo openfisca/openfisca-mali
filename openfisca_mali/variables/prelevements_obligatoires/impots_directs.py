4cc2767bf282502307eb75671091981b13ecf8c3
Léa <lea.ambroise1@gmail.com>
2018-09-20 17:35:28 +0200

    Création variable et paramètres pour l'ITS +test

--- openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
+++ openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
@@ -9,3 +9,23 @@
 from openfisca_core.model_api import *
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_mali.entities import *
+
+#On créé une variable pour le revenu net imposable
+class revenu_net_imposable(Variable):
+    value_type = float
+    entity = Person
+    label = u"Revenu net imposable pour une personne pour une année donnée"
+    definition_period = YEAR
+
+
+class impot_traitement_salaire(Variable):
+    value_type = float
+    entity = Person
+    label = u"Barême de l'ITS"
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        revenu_net_imposable = person('revenu_net_imposable', period)
+        bareme = parameters(period).prelevements_obligatoires.impots_directs.impot_traitement_salaire
+        impot = bareme.calc(revenu_net_imposable)
+        return impot
