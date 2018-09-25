98d26a796b238dcab53ddc882c4d7ed702ebdada
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-25 21:44:08 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 
 
-# 0.0.6 - [#8](https://github.com/openfisca/openfisca-mal/pull/8)
+# 0.0.7 - [#8](https://github.com/openfisca/openfisca-mal/pull/8)
 
 * Amélioration technique
 * Détails: configuration de CircleCI
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-Mali',
-    version='0.0.6',
+    version='0.0.7',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for Mali',
