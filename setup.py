d8cb12b677106035d259780be5cc13ff413ca6e8
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-12-13 22:35:10 +0100

    Fix dependancies

--- setup.py
+++ setup.py
@@ -18,13 +18,17 @@ setup(
     install_requires=[
         'OpenFisca-Core >= 25.2, < 26.0',
         ],
-    extras_require={
+    extras_require = {
         'dev': [
-            'flake8 >= 3.4.0, < 3.5.0',
-            'flake8-print',
-            'nose',
+            "autopep8 == 1.4.0",
+            "flake8 >= 3.5.0, < 3.6.0",
+            "flake8-print",
+            "pycodestyle >= 2.3.0, < 2.4.0",  # To avoid incompatibility with flake
+            "pytest < 4.0",
+            "scipy >= 0.17",  # Only used to test de_net_a_brut reform
+            "requests >= 2.8",
+            "yamllint >= 1.11.1, < 1.12",
             ]
         },
     packages=find_packages(),
-    test_suite='nose.collector',
     )
