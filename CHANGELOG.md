076f77d6a9191030a491128762860b19dc40f83c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-12-14 12:46:32 +0100

    Clean and fix test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+# 1.0.0 - [#8](https://github.com/openfisca/openfisca-mal/pull/8)
+
+* Ajoute impot_traitement_salaire
+* Détails: configuration de CircleCI
+
+
 # 0.0.8 - [#8](https://github.com/openfisca/openfisca-mal/pull/8)
 
 * Amélioration technique
--- openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
+++ openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
@@ -11,15 +11,16 @@ class impot_traitement_salaire(Variable):
     label = u"Barême de l'impôt pour les traitements et les salaires"
     definition_period = YEAR
 
+    def formula(person, period, parameters):
+        revenu_net_imposable = person('revenu_net_imposable', period)
+        bareme = parameters(period).prelevements_obligatoires.impots_directs.impot_traitement_salaire
+        print(bareme)
+        impot = bareme.calc(revenu_net_imposable)
+        return impot
+
 
 class revenu_net_imposable(Variable):
     value_type = float
     entity = Person
     label = u"Revenu net imposable"
     definition_period = YEAR
-
-    def formula(person, period, parameters):
-        revenu_net_imposable = person('revenu_net_imposable', period)
-        bareme = parameters(period).prelevements_obligatoires.impots_directs.impot_traitement_salaire
-        impot = bareme.calc(revenu_net_imposable)
-        return impot
