50e00743e599e585a6c4ee99f8435c6b40d6d590
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-12-25 23:04:43 +0100

    Working stata file import for mali

--- openfisca_mali/survey_scenarios.py
+++ openfisca_mali/survey_scenarios.py
@@ -9,17 +9,12 @@ from openfisca_core import periods
 from openfisca_mali import CountryTaxBenefitSystem as MaliTaxBenefitSystem
 
 from openfisca_survey_manager.scenarios import AbstractSurveyScenario
+from openfisca_survey_manager.utils import stata_files_to_data_frames
 
 
 class MaliSurveyScenario(AbstractSurveyScenario):
-    id_variable_by_entity_key = dict(
-        famille = 'id_famille',
-        )
-    role_variable_by_entity_key = dict(
-        famille = 'role_famille',
-        )
 
-    def __init__(self, tax_benefit_system = None, baseline_tax_benefit_system = None
+    def __init__(self, tax_benefit_system = None, baseline_tax_benefit_system = None,
             data = None, year = None):
         super(MaliSurveyScenario, self).__init__()
         if tax_benefit_system is None:
@@ -29,27 +24,16 @@ class MaliSurveyScenario(AbstractSurveyScenario):
             baseline_tax_benefit_system = baseline_tax_benefit_system,
             )
 
+        assert year is not None
+        self.year = year
+
         if data is None:
             return
 
-        stata_file_by_entity = data.get('stata_file_by_entity')
-
-        if stata_file_by_entity is None:
-            self.init_from_data(data = data)
-            return
-
-        assert year is not None
-        variables_from_stata_files = []
-        input_data_frame_by_entity_by_period = dict()
-        input_data_frame_by_entity_by_period[periods.period(period)] = input_data_frame_by_entity = dict()
-        for entity, file_path in stata_file_by_entity.items():
-            assert os.path.exists(file_path)
-            input_data_frame_by_entity[entity] = entity_data_frame = pd.read_stata(file_path)
-            variables_from_stata_files.append(list(entity_data_frame.columns))
-
+        variables_from_stata_files = stata_files_to_data_frames(data, period = year)
         self.used_as_input_variables = list(
             set(tax_benefit_system.variables.keys()).intersection(
                 set(variables_from_stata_files)
-                ))
-        data = dict(input_data_frame_by_entity_by_period = input_data_frame_by_entity_by_period)
+                )
+            )
         self.init_from_data(data = data)
--- openfisca_mali/tests/test_survey_scenario_from_stata_data.py
+++ openfisca_mali/tests/test_survey_scenario_from_stata_data.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 
 
+import os
+
+
 from openfisca_mali import CountryTaxBenefitSystem as MaliTaxBenefitSystem
+from openfisca_mali.survey_scenarios import MaliSurveyScenario
+
+from openfisca_mali.tests.data.fake_data_generator import data_directory
 
 
 def test_load_stata_data():
-    tax_benefit_system = MaliTaxBenefitSystem()
+    data = dict()
+    data['stata_file_by_entity'] = dict(
+        household = os.path.join(data_directory, 'household.dta'),
+        person = os.path.join(data_directory, 'person.dta'),
+        )
+    year = 2017
+    survey_scenario = MaliSurveyScenario(
+        tax_benefit_system = MaliTaxBenefitSystem(),
+        data = data,
+        year = year,
+        )
+    assert survey_scenario.calculate_variable('impot_traitement_salaire', period = year) == [5000]
 
+
+if __name__ == '__main__':
+    test_load_stata_data()
