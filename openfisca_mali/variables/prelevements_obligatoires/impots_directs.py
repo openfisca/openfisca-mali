1d3c4e4a8497448b14b4801694edc6a73164e3a6
nirirazaka <razakamanana@dial.prd.fr>
2019-02-04 17:40:27 +0100

    add impot_brut & impot_traitement_salaire

--- openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
+++ openfisca_mali/variables/prelevements_obligatoires/impots_directs.py
@@ -8,18 +8,23 @@ from openfisca_mali.entities import *
 class impot_traitement_salaire(Variable):
     value_type = float
     entity = Person
-    label = u"Barême de l'impôt pour les traitements et les salaires"
+    label = u"Impôt pour les traitements et les salaires"
     definition_period = YEAR
 
     def formula(person, period, parameters):
-        revenu_net_imposable = person('revenu_net_imposable', period)
-        bareme = parameters(period).prelevements_obligatoires.impots_directs.impot_traitement_salaire
-        impot = bareme.calc(revenu_net_imposable)
-        return impot
+        impot_brut = person('impot_brut', period)
+        impot_traitement_salaire = impot_brut * person.household('reductions_familiales', period)
+        return impot_traitement_salaire
 
 
-class revenu_net_imposable(Variable):
+class impot_brut(Variable):
     value_type = float
     entity = Person
-    label = u"Revenu net imposable"
+    label = u"Impôt brut"
     definition_period = YEAR
+
+    def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        bareme = parameters(period).prelevements_obligatoires.impots_directs.impot_traitement_salaire
+        impot_brut = bareme.calc(salaire)
+        return impot_brut
